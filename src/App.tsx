f86965d046f9be77d7c54dee2c6079ac7221ee3e
August Skarsfjord Nyheim <augustsn@stud.ntnu.no>
2023-10-10 12:42:00 +0200

    #1 Installed Chakri, icons and created layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Grid
+    templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`,
+    }}
+    templateColumns={{
+      base: "1fr",
+      lg: "150px 1fr",
+    }}
+    >
+      <GridItem area="nav" bgColor="yellow">
+        <Navbar></Navbar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5} bgColor="orange">
+          <h1>Aside</h1>
+        </GridItem>
+      </Show>
+      <GridItem area="main" bgColor="red">
+        <h1>main</h1>
+      </GridItem>
+    </Grid>
   )
 }
 
