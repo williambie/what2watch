faa84095dc927030cab62b6b11f9ba0b4c9ae1f1
August Skarsfjord Nyheim <augustsn@stud.ntnu.no>
2023-10-31 22:33:48 +0100

    #34 Removed old sorting/filtering

--- frontend/src/components/Main/MovieGrid/MovieGrid.tsx
+++ frontend/src/components/Main/MovieGrid/MovieGrid.tsx
@@ -4,37 +4,12 @@ import { Movie } from "../../../types/types";
 import LoadingCard from "../LoadingCard/LoadingCard";
 
 interface Props {
-  genre?: number | null;
-  sortBy?: string;
   movies: Movie[];
   loading: boolean;
 }
 
 // MovieGrid is a grid that displays all movies
-const MovieGrid = ({ movies, genre, sortBy, loading }: Props) => {
-  const filteredMovies = genre
-    ? movies.filter((movie) => movie.genre_ids.includes(genre))
-    : movies;
-
-  // Sorts movies by vote average, title or popularity
-  const sortMovies = (movies: Movie[], sortBy: string) => {
-    const moviesCopy = [...movies];
-
-    return moviesCopy.sort((a: Movie, b: Movie) => {
-      if (sortBy === "vote_average") {
-        return b.vote_average - a.vote_average;
-      } else if (sortBy === "title") {
-        return a.title.localeCompare(b.title);
-      } else if (sortBy === "popularity") {
-        return b.popularity - a.popularity;
-      }
-      return 0;
-    });
-  };
-
-  const sortedMovies = sortBy
-    ? sortMovies(filteredMovies, sortBy)
-    : filteredMovies;
+const MovieGrid = ({ movies, loading }: Props) => {
 
   return (
     <SimpleGrid
@@ -46,7 +21,7 @@ const MovieGrid = ({ movies, genre, sortBy, loading }: Props) => {
         ? Array(15)
             .fill(0)
             .map((_, idx) => <LoadingCard key={idx} />)
-        : sortedMovies.map((movie: Movie) => (
+        : movies.map((movie: Movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
     </SimpleGrid>
